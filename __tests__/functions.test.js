5bc61426adf59719aa9df799eaea08ecc438f8bb
Cherno Badjan <badjancherno@gmail.com>
2021-04-25 11:52:06 -0700

    munge functions written

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -1,10 +1,10 @@
-// const pool = require('../lib/utils/pool');
-// const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
-// describe('destinare-be routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
-// });
+describe('destinare-be routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+});
